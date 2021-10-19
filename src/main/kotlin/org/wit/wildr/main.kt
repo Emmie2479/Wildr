b40a2b7ac35580b6c11d738af8324c1705d011fb
Emmie2479 <60179669+Emmie2479@users.noreply.github.com>
2021-10-19 16:56:33 +0100

    commit menue repeat

--- src/main/kotlin/org/wit/wildr/main.kt
+++ src/main/kotlin/org/wit/wildr/main.kt
@@ -4,8 +4,38 @@ import mu.KotlinLogging
 
 private val logger = KotlinLogging.logger {}
 
-fun main(args: Array<String>){
+fun main(args: Array<String>) {
     logger.info { "Launching Wildr App" }
     println("Wildr Kotlin App Version 1.0")
+
+    var input: Int
+
+    do {
+        input = menu()
+        when(input) {
+            1 -> println("You pick the species type your looking for")
+            -1 -> println("Leaving Wildr")
+            else -> println("Not an Option")
+        }
+        println()
+    } while (input != -1)
+    logger.info { "Shutting Down Wildr App" }
 }
 
+fun menu() : Int {
+
+    var option : Int
+    var input: String? = null
+
+    println("Home")
+    println("Carnivores")
+    println("Herbivores")
+    println("Other species")
+    println("Return")
+    input = readLine()!!
+    option = if (input.toIntOrNull() != null && !input.isEmpty())
+        input.toInt()
+    else
+        -9
+    return option
+}
