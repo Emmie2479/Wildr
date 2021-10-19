cc6d80ead1513bb5d40f306369c3a17a582a7d7a
Emmie2479 <60179669+Emmie2479@users.noreply.github.com>
2021-10-19 15:45:47 +0100

    commit next section

--- src/main/kotlin/org/wit/wildr/main.kt
+++ src/main/kotlin/org/wit/wildr/main.kt
@@ -1,5 +1,11 @@
 package org.wit.wildr
 
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
+
 fun main(args: Array<String>){
+    logger.info { "Launching Wildr App" }
     println("Wildr Kotlin App Version 1.0")
 }
+
